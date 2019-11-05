64f40ff6a4de84e3d56c5dbaff53ff220a109d8b
Ivan Bilyarov <3bilyi81@solent.ac.uk>
2019-11-05 16:54:07 +0000

    Week7Tutorial

---
+++ Week7Tutorial/src/week7tutorial/Week7Tutorial.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week7tutorial;
+
+/**
+ *
+ * @author 3bilyi81
+ */
+public class Week7Tutorial {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
